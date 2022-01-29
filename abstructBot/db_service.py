4dc46841016ad73c5685bf413e7262373cc4d444
夜鷹ねりあ <nerianighthawk@yotakanerianoMac-mini.local>
2022-01-30 03:23:23 +0900

    正常系機能完成

---
+++ abstructBot/db_service.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+db_name = 'SPACEDOG.db'
+
+
+def add_member(content_list, user_id):
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+
+    for content in content_list:
+        cur.execute(f'INSERT INTO contents(discord_id, content) values({user_id}, "{content}")')
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+def read_contents(user_id):
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+
+    cur.execute(f'SELECT * FROM contents WHERE discord_id={user_id}')
+    contents_list = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return contents_list
+
+
+def init_setting():
+    conn = sqlite3.connect(db_name)
+    
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE contents(id INTEGER PRIMARY KEY AUTOINCREMENT, discord_id INTEGER, content STRING)')
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
